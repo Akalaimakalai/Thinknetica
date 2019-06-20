f00cf0ffd4e25bfe4bb5be0583d79e86e34a304b
Ar4i <jackshaakara@gmail.com>
2019-06-20 21:11:26 +0300

    not for examination

--- lesson-09/Modules/new.rb
+++ lesson-09/Modules/new.rb
@@ -1,23 +1,31 @@
-require_relative 'validation'
+require_relative 'validation2'
 
 class Foo
   include Validation
 
-  attr_reader :name
+  attr_reader :name, :val_arr
+
+  validate :name, :presence
+  validate :name, :format, /^( |[a-z]){5,20}$/i
   
   FORMAT = /^( |[a-z]){5,20}$/i
 
   def initialize(name)
     @name = name
+    validate!
   end
+
+  
 end
 
-foo = Foo.new("ololosh")
+foo = Foo.new("ololsh")
 
-puts foo.class.validate(foo.name, :presence)
-puts foo.class.validate(foo.name, :format)
-puts foo.class.validate(foo.class, :type)
 
-#foo.validate!
 
-puts foo.valid?
+# puts foo.class.validate(foo.name, :presence)
+# puts foo.class.validate(foo.name, :format)
+# puts foo.class.validate(foo.class, :type)
+
+# #foo.validate!
+
+# puts foo.valid?
--- lesson-09/Modules/validation.rb
+++ lesson-09/Modules/validation.rb
@@ -6,7 +6,7 @@ module Validation
 
   module ClassMethods
     def validate(name, validate_type)
-      a = proc { |name| return true if (name.strip != '') && (!name.nil?) }
+      a = proc { |name| return true if (name != '') && (!name.nil?) }
       b = proc { |name| return true if name =~ self::FORMAT }
       c = proc { |name| return true if name == self }
       hash = {
@@ -21,59 +21,19 @@ module Validation
 
   module InstanceMethods
     def valid?
-      validate!
+      validate!(self.name)
       true
-    rescue StandardError
+    rescue RuntimeError
       false
     end
 
     private
 
     def validate!(variable)
-      raise 'Presence error' unless self.class.validate(variable, :presence)
+      raise 'Presence error' unless self.class.validate(variable.strip, :presence)
       raise 'Format error' unless self.class.validate(variable, :format)
       raise 'Type error' unless self.class.validate(self.class, :type)
       true
     end
   end
 end
-
-# module Validation
-#   def self.validate(name, validate_type, *additional)
-#     hash = {
-#       presence: "presence?",
-#       format: "format?",
-#       type: "type?"
-#     }
-#     send(hash[validate_type])(name, additional.first)
-#   end
-
-#   def valid? (name, my_format, my_class)
-#     return true if validate!(name, my_format, my_class)
-#     false
-#   end
-
-#   private
-
-#   def validate! (name, my_format, my_class)
-#     raise "Presence error" unless self.class.validate(name, :presence)
-#     raise "Format error" unless self.class.validate(name, :format, my_format)
-#     raise "Type error" unless self.class.validate(name, :type, my_class)
-#     true
-#   end
-
-#   def presence?(*name)
-#     return true if (name.first.strip != '') && (name.first != nil)
-#     false
-#   end
-
-#   def format?(name, my_format)
-#     return true if name ~= my_format
-#     false
-#   end
-
-#   def type?(name, my_class)
-#     return true if name.class == my_class
-#     false
-#   end
-# end
