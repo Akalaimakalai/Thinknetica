4c4018c31e5a8a3946ab19d869bfe193436cac6c
Ar4i <jackshaakara@gmail.com>
2019-06-21 13:42:47 +0300

    repair accessor

--- lesson-09/Modules/acсessors.rb
+++ lesson-09/Modules/acсessors.rb
@@ -12,9 +12,13 @@ module Acсessors
 
         var_arr = "@#{name}_arr"
         define_method("#{name}=".to_sym) do |value|
-          instance_variable_set(var_arr, []) if instance_variable_get(var_arr).nil?
-          instance_variable_set(var_name, value)
-          instance_variable_get(var_arr) << value
+          if instance_variable_get(var_arr).nil?
+            instance_variable_set(var_arr, [])
+            instance_variable_set(var_name, value)
+          elsif instance_variable_get(var_name) != value
+            instance_variable_get(var_arr) << instance_variable_get(var_name)
+            instance_variable_set(var_name, value)
+          end
         end
 
         define_method("#{name}_history".to_sym) { instance_variable_get(var_arr) }
@@ -28,9 +32,13 @@ module Acсessors
       var_arr = "@#{name}_arr"
       define_method("#{name}=".to_sym) do |value|
         raise 'Wrong class of argement!!!' if value.class != arg_class
-        instance_variable_set(var_arr, []) if instance_variable_get(var_arr).nil?
-        instance_variable_set(var_name, value)
-        instance_variable_get(var_arr) << value
+        if instance_variable_get(var_arr).nil?
+          instance_variable_set(var_arr, [])
+          instance_variable_set(var_name, value)
+        elsif instance_variable_get(var_name) != value
+          instance_variable_get(var_arr) << instance_variable_get(var_name)
+          instance_variable_set(var_name, value)
+        end
       end
 
       define_method("#{name}_history".to_sym) { instance_variable_get(var_arr) }
--- lesson-09/Modules/test.rb
+++ lesson-09/Modules/test.rb
@@ -13,44 +13,81 @@ test.a = 1
 test.b = 2
 test.c = 3
 
-test.a = 34
-test.b = 5
-test.c = 16
-
-test.a = :ololosh
-test.b = "kek"
-test.c = nil
-
-test.a = 8
-test.b = 11
-test.c = 66
-
-#test.b_array
-
-print "#{test.instance_variables}\n"
-
-puts test.instance_variable_get(:@a)
-test.instance_variable_set(:@a, 17)
-puts test.instance_variable_get(:@a)
-
-print "#{test.instance_variable_get(:@a_arr)}\n"
-print "#{test.instance_variable_get(:@b_arr)}\n"
-print "#{test.instance_variable_get(:@c_arr)}\n"
+puts test.a
+puts test.b
+puts test.c
 
 print "#{test.a_history}\n"
 print "#{test.b_history}\n"
 print "#{test.c_history}\n"
 
-test.d = 5
+test.a = 1
+test.b = 5
+test.c = 16
 
-puts test.instance_variable_get(:@d)
-test.instance_variable_set(:@d, 42)
-puts test.instance_variable_get(:@d)
+puts test.a
+puts test.b
+puts test.c
 
-test.d = 33
-test.d = 68
+print "#{test.a_history}\n"
+print "#{test.b_history}\n"
+print "#{test.c_history}\n"
 
-print "#{test.d_history}\n"
+test.a = :ololosh
+test.b = "kek"
+test.c = nil
 
-test.d = "lol"
+puts test.a
+puts test.b
+puts test.c
+
+print "#{test.a_history}\n"
+print "#{test.b_history}\n"
+print "#{test.c_history}\n"
+
+
+test.a = 8
+test.b = 11
+test.c = 66
+
+puts test.a
+puts test.b
+puts test.c
+
+print "#{test.a_history}\n"
+print "#{test.b_history}\n"
+print "#{test.c_history}\n"
+
+
+#test.b_array
+
+# print "#{test.instance_variables}\n"
+
+# puts test.instance_variable_get(:@a)
+# test.instance_variable_set(:@a, 17)
+# puts test.instance_variable_get(:@a)
+
+# print "#{test.instance_variable_get(:@a_arr)}\n"
+# print "#{test.instance_variable_get(:@b_arr)}\n"
+# print "#{test.instance_variable_get(:@c_arr)}\n"
+
+# print "#{test.a_history}\n"
+# print "#{test.b_history}\n"
+# print "#{test.c_history}\n"
+
+# puts test.a
+# puts test.b
+# puts test.c
+# test.d = 5
+
+# puts test.instance_variable_get(:@d)
+# test.instance_variable_set(:@d, 42)
+# puts test.instance_variable_get(:@d)
+
+# test.d = 33
+# test.d = 68
+
+# print "#{test.d_history}\n"
+
+# test.d = "lol"
 
